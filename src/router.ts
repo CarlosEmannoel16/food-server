a96ac8d5f05cf3abada6daff8c84cfc505902867
CarlosEmannoel <carlosemannoel2019@gmail.com>
2022-08-11 23:11:27 -0300

    alteração na estrutura v3

--- src/router.ts
+++ src/router.ts
@@ -6,12 +6,12 @@ const router = express.Router();
 router.use(express.json())
 
 //CLIENTE
-router.get("/cliente",  controllers.clienteController.listar)
-router.post("/cliente/create", controllers.clienteController.criar)
-router.put("/cliente", controllers.clienteController.atualizar)
+router.get("/cliente",  controllers.ClienteController.listar)
+router.post("/cliente/create", controllers.ClienteController.criar)
+router.put("/cliente", controllers.ClienteController.atualizar)
 
 //AUTENTICAÇÃO
-router.get("/auth", controllers.authController.loginController)
+router.get("/auth", controllers.AuthController.loginController)
 
 //PRATOS
 
