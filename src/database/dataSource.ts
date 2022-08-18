d60cf63c4ad5f3787246f7ae984db63c826279ea
CarlosEmannoel <carlosemannoel2019@gmail.com>
2022-08-18 08:48:09 -0300

    configurando .env

--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -1,16 +1,29 @@
-import {DataSource} from 'typeorm'
+import { DataSource } from 'typeorm'
 import Adm from './entityes/Adm'
 import Cliente from './entityes/Cliente'
 import Endereco from './entityes/Endereco'
 import Prato from './entityes/Prato'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+// const dataSource = new DataSource({
+// //     type: "postgres",
+// //     host: "localhost",
+// //     port: 5432,
+// //     username: "postgres",
+// //     password: "surane2210",
+// //     database: "postgres",
+// //     entities: [Cliente, Prato, Endereco, Adm]
+// // })
 
 const dataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "surane2210",
-    database: "postgres",
+    host: process.env.HOST_HEROKU,
+    port: parseInt(process.env.PORT_HEROKU as string),
+    username: process.env.USERNAME_HEROKU,
+    password: process.env.PASSWORD_HEROKU,
+    database: process.env.DATABASE_HEROKU,
     entities: [Cliente, Prato, Endereco, Adm]
 })
 
